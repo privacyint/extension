6aadbf86818e52fa7cc0ed62454949b6186c756e
Mustafa <mus@musalbas.com>
2014-07-18 00:16:48 +0100

    banner base

--- data/gchq.js
+++ data/gchq.js
@@ -1,3 +1,13 @@
+function displayBanner() {
+    document.body.innerHTML = '\
+<div style="margin: 0; padding: 0; position: relative; top: 0; left: 0; display: block; z-index: 1000">\
+    <div style="margin: 0; padding: 10px; border-bottom: 1px solid #000000; font-weight: normal; text-align: left">\
+        <p>blab</p>\
+    </div>\
+</div>\
+    ' + document.body.innerHTML;
+}
+
 $(document).ready(function() {
-    alert('test');
+    displayBanner();
 });
