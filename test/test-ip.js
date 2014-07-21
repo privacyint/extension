3dcdede6fa4e3d43ff87efd20da415f0a8facf63
Mustafa <mus@musalbas.com>
2014-07-21 23:37:43 +0100

    meaningful test messages

--- test/test-ip.js
+++ test/test-ip.js
@@ -1,7 +1,13 @@
 var ip = require('./ip.js');
 
+exports['test isNonGBHost'] = function(assert) {
+    assert.ok(ip.isNonGBHost('google.com') == true, "works; googe.com is non-GB");
+    assert.ok(ip.isNonGBHost('bbc.co.uk') == false, "works; bbc.co.uk is GB");
+    assert.ok(ip.isNonGBHost('10.1.1.1') == false, "works; 10.1.1.1 is GB");
+}
+
 exports['test ipv4StringToInt'] = function(assert) {
-    assert.ok(ip.ipv4StringToInt('1.3.3.7') == 16974599, "works");
+    assert.ok(ip.ipv4StringToInt('1.3.3.7') == 16974599, "works; 1.3.3.7 is 16974599 as an int");
 }
 
 require('sdk/test').run(exports);
