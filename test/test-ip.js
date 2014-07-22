cb9d96974652f5103bfe555c04753858882fd83f
Mustafa <mus@musalbas.com>
2014-07-22 10:15:27 +0100

    fix non-existent domains error; add tests

--- test/test-ip.js
+++ test/test-ip.js
@@ -1,13 +1,14 @@
 var ip = require('./ip.js');
 
 exports['test isNonGBHost'] = function(assert) {
-    assert.ok(ip.isNonGBHost('google.com') == true, "works; googe.com is non-GB");
-    assert.ok(ip.isNonGBHost('bbc.co.uk') == false, "works; bbc.co.uk is GB");
-    assert.ok(ip.isNonGBHost('10.1.1.1') == false, "works; 10.1.1.1 is GB");
-}
-
-exports['test ipv4StringToInt'] = function(assert) {
-    assert.ok(ip.ipv4StringToInt('1.3.3.7') == 16974599, "works; 1.3.3.7 is 16974599 as an int");
+    assert.ok(ip.isNonGBHost('google.com') == true, "googe.com is non-GB");
+    assert.ok(ip.isNonGBHost('bbc.co.uk') == false, "bbc.co.uk is GB");
+    assert.ok(ip.isNonGBHost('212.58.246.104') == false, "212.58.246.104 is GB");
+    assert.ok(ip.isNonGBHost('10.1.1.1') == false, "10.1.1.1 is GB");
+    assert.ok(ip.isNonGBHost('ipv6.google.com') == true, "ipv6.google.com is non-GB");
+    assert.ok(ip.isNonGBHost('2a00:2381:ffff::1') == false, "2a00:2381:ffff::1 is GB");
+    assert.ok(ip.isNonGBHost('::1') == false, "::1 is GB");
+    assert.ok(ip.isNonGBHost('blab.blab') == false, "blab.blab (non-existent) is GB");
 }
 
 require('sdk/test').run(exports);
--- test/test-rangesearch.js
+++ test/test-rangesearch.js
@@ -2,10 +2,10 @@ var rangesearch = require('./rangesearch.js');
 var ipranges = require('./ipranges.js');
 
 exports['test binaryRangeIndexOf'] = function(assert) {
-    assert.ok(rangesearch.binaryRangeIndexOf(34999299, ipranges.GBIPv4Ranges) > -1, "works; GB IPv4 address found in GB IPv4 address range");
-    assert.ok(rangesearch.binaryRangeIndexOf(34999290, ipranges.GBIPv4Ranges) == -1, "works; non-GB IPv4 address not found in GB IPv4 address range");
-    assert.ok(rangesearch.binaryRangeIndexOf("42540613659385265355261104598917578755", ipranges.GBIPv6Ranges, useBigInt=true) > -1, "works; GB IPv6 address found in GB IPv6 address range");
-    assert.ok(rangesearch.binaryRangeIndexOf("42540619363817802085571869850780827646", ipranges.GBIPv6Ranges, useBigInt=true) == -1, "works; non-GB IPv6 address not found in GB IPv6 range");
+    assert.ok(rangesearch.binaryRangeIndexOf(34999299, ipranges.GBIPv4Ranges) > -1, "GB IPv4 address found in GB IPv4 address range");
+    assert.ok(rangesearch.binaryRangeIndexOf(34999290, ipranges.GBIPv4Ranges) == -1, "non-GB IPv4 address not found in GB IPv4 address range");
+    assert.ok(rangesearch.binaryRangeIndexOf("42540613659385265355261104598917578755", ipranges.GBIPv6Ranges, useBigInt=true) > -1, "GB IPv6 address found in GB IPv6 address range");
+    assert.ok(rangesearch.binaryRangeIndexOf("42540619363817802085571869850780827646", ipranges.GBIPv6Ranges, useBigInt=true) == -1, "non-GB IPv6 address not found in GB IPv6 range");
 }
 
 require('sdk/test').run(exports);
