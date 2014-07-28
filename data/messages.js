98836c62ef3c7263509d573f892d63d6c2681c88
Mustafa <mus@musalbas.com>
2014-07-28 16:55:12 +0100

    make box slightly bigger, reword generic message

--- data/messages.js
+++ data/messages.js
@@ -1,7 +1,7 @@
 messages = {
 
     // site-unspecific messages (site is "external")
-    generic_t1: 'This website is hosted outside the UK. GCHQ have antiquated rules that say if it’s outside the UK, this communication is fair game for spying. Why should it matter where a server is located for your fundamental human rights to be respected?',
+    generic_t1: 'It seems this site is hosted outside the UK. GCHQ have antiquated rules that say if it’s outside the UK, this communication is fair for spying. Why should it matter where a server is located for your fundamental human rights to be respected?',
 
     // site-unspecific messages (site uses resources from "external" sites)
     generic_t2: 'Servers both inside the UK and outside are being used to create this website. Secret interpretations of opaque laws will govern what GCHQ can do with this.',
