ad73759635061099273c68810d5f706d27b7ff0f
Mustafa <mus@musalbas.com>
2015-01-09 16:25:05 +0000

    Fix messages

--- data/messages.js
+++ data/messages.js
@@ -5,14 +5,14 @@ messages = {
 
     // site-unspecific messages (site is "external")
     generic_t1: [
-        "Psst! It seems this site is hosted outside the UK. GCHQ have byzantine rules that say if the site your visiting is outside the UK, they don't need an individual warrant to intercept your communications.",
-        "GCHQ's stated aim is to \"Master the internet\". Because this website it hosted outside the UK, TEMPORA's surveillance engines are likely processing what you're doing right now.",
+        "Psst! It seems this site is hosted outside the UK. GCHQ have byzantine rules that say if the site you're visiting is outside the UK, they don't need an individual warrant to intercept your communications.",
+        "GCHQ's stated aim is to \"Master the internet\". Because this website is hosted outside the UK, TEMPORA's surveillance engines are likely processing what you're doing right now.",
         "GCHQ has said it wants to \"collect it all\". That includes intercepting information on the site you are visiting right now, since it's hosted outside the UK.",
         "GCHQ wants to \"Master the internet\". In one 24-hour period in 2011, more than 39bn events were recorded by GCHQ. They can do this beacause an individual warrant is not needed to intercept communications like this.",
         ],
 
     // site-unspecific messages (site uses resources from "external" sites)
-    generic_t2: "Servers both inside the UK and outside are being used to create this website. Secret interpretations of opaque laws will govern what GCHQ can do with this. If they deem it outside the UK, they don't need an individual warrant to intercept your communications.",
+    generic_t2: "Servers both inside and outside the UK are being used to create this website. Secret interpretations of opaque laws will govern what GCHQ can do with this. If they deem it outside the UK, they don't need an individual warrant to intercept your communications.",
 
     // site-specific messages
     'google.com': "Your searches on Google could be intercepted by GCHQ without an individual warrant, since Google is hosted outside the UK and our intelligence agencies classify it as an 'external communication'.",
@@ -20,11 +20,11 @@ messages = {
     'facebook.com': "According to GCHQ, you're communicating with Facebook, and not your friends. Since communicating with Facebook is an 'external communication', they can intercept your communications without an individual warrant.",
     'twitter.com': "GCHQ hoovers up as many tweets as it can to do pattern analysis before tipping off police if they see something they don't like.",
     'wikipedia.org': "GCHQ can intercept who visits what Wikipedia pages without an individual warrant, and they consider them 'external communications'. Why should your fundamental rights be infinged when reading the world's largest library instead of your local one?",
-    'youtube.com': "Whether a how-to guide or a cute cat video, GCHQ thinks it can spy on what you're watching on YouTube without an individual warrant since the site's hosted outside the UK.",
+    'youtube.com': "Whether a how-to guide or a cute cat video, GCHQ thinks it can spy on what you're watching on YouTube without an individual warrant since the site is hosted outside the UK.",
     'bbc.co.uk': "BBC uses servers both inside and outside the UK to create this website. Secret interpretations of opaque laws will govern what GCHQ can do with this. If they deem it outside the UK, they don't need an individual warrant to spy on your communications.",
-    'ebay.co.uk': "You're trying to bid on something, not have GCHQ intercept your communications. But since Ebay's server is located outside the UK, GCHQ deem this an 'external communication' and don't need an individual warrant.",
-    'amazon.co.uk': "You're trying to buy something, not have GCHQ intercept your communications. But since Amazon's servers are located outside the UK, GCHQ deem this an 'external communication' and don't need an individual warrant.",
-    'amazon.com': "You're trying to buy something, not have GCHQ intercept your communications. But since Amazon's servers are located outside the UK, GCHQ deem this an 'external communication' and don't need an individual warrant.",
+    'ebay.co.uk': "You're trying to bid on something, not have GCHQ intercept your communications. But since Ebay's server is located outside the UK, GCHQ deem this an 'external communication' and don't need an individual warrant to spy on you.",
+    'amazon.co.uk': "You're trying to buy something, not have GCHQ intercept your communications. But since Amazon's servers are located outside the UK, GCHQ deem this an 'external communication' and don't need an individual warrant to spy on you.",
+    'amazon.com': "You're trying to buy something, not have GCHQ intercept your communications. But since Amazon's servers are located outside the UK, GCHQ deem this an 'external communication' and don't need an individual warrant to spy on you.",
     'reddit.com': "By reading 'The front page of the internet', your communications could be intercepted by GCHQ without an individual warrant. That's because Reddit's servers are located outside the UK.",
     'buzzfeed.com': "Top three reasons why GCHQ can intercept this communication: This site is hosted outside the UK, they say they don't need an individual warrant, and they've stated their aim to be to \"Collect it All\".",
     'imdb.com': "Lights! Camera! Intercept! This site is located outside the UK, so while you're browsing about movies, GCHQ can intercept this communication without an individual warrant. Talk about a bad ending.",
